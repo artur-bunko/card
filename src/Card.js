2439a3dbfed93a1de901b91151252b686c8a7ab3
artur-bunko <artur.bunko@storeworkflows.com>
2021-02-17 13:25:15 +0200

    feat: attach dispatch

--- src/Card.js
+++ src/Card.js
@@ -1,40 +1,46 @@
 import classnames from "classnames";
 import propTypes from "prop-types";
 import styles from "./styles.scss";
-import {dispatch} from "./utils/dispatch";
-import {getParentNode} from "./utils/getParent"
-import {useEffect} from "react";
+import {attachDispatch} from "./utils/getParent"
+import {Component} from "react";
+import CardPreloader from "./Preloader";
 
+@attachDispatch()
+class Card extends Component {
+	constructor(props) {
+		super(props);
+	}
 
-function Card (props) {
-	const { vertical, loading, customStyles, size } = props;
+	render() {
+		const { vertical, loading, customStyles, size } = this.props;
 
-	useEffect(() => {
-		const node = getParentNode(this);
-		console.log(node)
-	})
+		console.log(this.props);
 
-	return (
-		<>
-			<style type="text/css">{styles}</style>
-			<div className={
-				classnames({
-					"swf-card": true,
-					"vertical": vertical,
-					[`--${size}`]: true
-				})
-			} style={customStyles}>
-				<button onClick={() => dispatch("TEST_DISPATCH", {Log: "adadsdasd"})}>Dispatch</button>
-			</div>
-		</>
-	)
+		if (loading) return <CardPreloader/>
+
+		return (
+			<>
+				<style type="text/css">{styles}</style>
+				<div className={
+					classnames({
+						"swf-card": true,
+						"vertical": vertical,
+						[`--${size}`]: true
+					})
+				} style={customStyles}>
+					<button onClick={() => this.props.dispatch("TEST_DISPATCH", {Log: "adadsdasd"})}>Dispatch</button>
+				</div>
+			</>
+		)
+	}
 }
 
 Card.propTypes = {
 	vertical: propTypes.bool,
 	loading: propTypes.bool,
 	customStyles: propTypes.object,
-	size: propTypes.oneOf(["s", "m", "l"])
+	size: propTypes.oneOf(["s", "m", "l"]),
+	dispatch: propTypes.func
 }
 
 export default Card
--- src/utils/getParent.js
+++ src/utils/getParent.js
@@ -1,4 +1,33 @@
+import * as React from "react";
 import ReactDOM from "react-dom";
-export function getParentNode(node = null) {
-    return ReactDOM.findDOMNode(node)
+
+class ContainerIdDetector extends React.Component {
+    constructor() {
+        super();
+        this.state = { dispatch: () => void 0 };
+    }
+
+    componentDidMount() {
+        const currentNode = ReactDOM.findDOMNode(this);
+        const host = currentNode.getRootNode().host;
+        const dispatch = host.dispatch;
+
+        this.setState({
+            dispatch
+        })
+    }
+
+    render() {
+        return React.cloneElement(
+            React.Children.only(this.props.children),
+            {dispatch: this.state.dispatch}
+        );
+    }
+}
+
+export function attachDispatch() {
+    return (Component) => (props) =>
+        <ContainerIdDetector>
+            <Component {...props} />
+        </ContainerIdDetector>
 }
